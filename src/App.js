a96f60b825899d730a40f8ba4c40853130f41c43
VKIM <vadim.siebelcrm@gmail.com>
2025-02-19 23:22:44 +0500

    add opty

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    fetch("https://palvenko-production.up.railway.app/opty", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "*/*"
+      }
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`Ошибка HTTP: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => console.log("Ответ от сервера:", data))
+      .catch(error => console.error("Ошибка запроса:", error));
+  }, []); // Пустой массив зависимостей → выполняется только при первом рендере
+    
   return (
     <div className="App">
       <header className="App-header">
