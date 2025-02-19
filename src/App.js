372d858c145bf6e850d9b052d0c6862b1395c630
VKIM <vadim.siebelcrm@gmail.com>
2025-02-19 21:53:25 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -18,6 +18,10 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        <h1>Привет, React!</h1>
+        <button onClick={() => alert("Кнопка нажата!")}>Нажми меня</button>
+      </div>
     </div>
   );
 }
