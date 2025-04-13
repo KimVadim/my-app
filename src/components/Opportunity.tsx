81e687a489ffc7ce18eaed55e4ba35159cf4a8e2
VKIM <vadim.siebelcrm@gmail.com>
2025-04-13 21:44:59 +0500

    rollback cra

--- src/components/Opportunity.tsx
+++ src/components/Opportunity.tsx
@@ -9,6 +9,7 @@ import { opportunityMeta } from "./AllApplicationMeta.tsx";
 import type { MenuProps } from 'antd';
 import { SettingOutlined } from '@ant-design/icons';
 import '../App.css';
+import { Line } from '@ant-design/charts';
 
 type MenuItem = Required<MenuProps>['items'][number];
 
@@ -62,6 +63,47 @@ export const Opportunity: React.FC = () => {
     setCurrent(e.key);
   };
 
+  const data = [
+    { year: '1991', value: 3 },
+    { year: '1992', value: 4 },
+    { year: '1993', value: 3.5 },
+    { year: '1994', value: 5 },
+    { year: '1995', value: 4.9 },
+    { year: '1996', value: 6 },
+    { year: '1997', value: 7 },
+    { year: '1998', value: 9 },
+    { year: '1999', value: 13 },
+  ];
+  const config = {
+    data,
+    height: 400,
+    xField: 'year',
+    yField: 'value',
+    point: {
+      size: 5,
+      shape: 'circle',
+    },
+    tooltip: {
+      formatter: (data) => {
+        return {
+          name: '',
+          value: String,
+        };
+      },
+      customContent: (name, data) =>
+        `<div>${data?.map((item) => {
+          return `<div class="tooltip-chart" >
+              <span class="tooltip-item-name">${item?.name}</span>
+              <span class="tooltip-item-value">${item?.value}</span>
+            </div>`;
+        })}</div>`,
+      showMarkers: Boolean,
+      showContent: Boolean,
+      position: 'right | left',
+      showCrosshairs: Boolean,
+    },
+  };
+
   return (
     <>
       {contextHolder}
@@ -109,6 +151,7 @@ export const Opportunity: React.FC = () => {
             onClick: () => handleRowClick(record),
           })}
         />
+        <Line {...config} />
       </Spin>
       {isModalOpen && 
         <OpportunityModal
